03904686263736ef83059e9ea6fc422c5f6eaba3
Chenguang Xu <xcg1234@gmail.com>
2015-07-08 16:55:22 -0400

    Deduction Table

--- app/scripts/controllers/deductionCtrl.js
+++ app/scripts/controllers/deductionCtrl.js
@@ -1,10 +1,17 @@
 'use strict';
 
 angular.module('gbApp')
-  .controller('deductionCtrl', function($scope, getUserService) {
+  .controller('deductionCtrl', function($scope, $http, getUserService) {
     getUserService.getUser().$promise.then(function(response) {
       $scope.userName = response.userName;
     });
-
+    $http.get('/api/deduction').
+      success(function(data){
+        $scope.deductions = data.deductions;
+        console.log(data);
+      }).
+      error(function(data,status,headers,config){
+        console.log("ERROR")
+      })
     console.log('deduction state');
   });
