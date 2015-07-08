100e32dcf058d73d934ca27f4ba13440e4d32c66
Steven <sandysu@gmail.com>
2015-07-08 15:50:00 -0400

    Second step to fix the ajax call and css styling

--- app/scripts/controllers/deductionCtrl.js
+++ app/scripts/controllers/deductionCtrl.js
@@ -1,21 +1,30 @@
 'use strict';
 
 angular.module('gbApp')
-    .controller('deductionCtrl', function($scope, getUserService) {
+    .controller('deductionCtrl', function($scope, $http, getUserService) {
         getUserService.getUser().$promise.then(function(response) {
             $scope.userName = response.userName;
         });
 
-        $scope.deductions = [{
-            'name': 'Nexus S',
-            'shortDiscription': 'Fast just got faster with Nexus S.'
-        }, {
-            'name': 'Motorola XOOM™ with Wi-Fi',
-            'shortDiscription': 'The Next, Next Generation tablet.'
-        }, {
-            'name': 'MOTOROLA XOOM™',
-            'shortDiscription': 'The Next, Next Generation tablet.'
-        }];
+        $http.get('/api/deduction').
+        success(function(data) {
+            $scope.deductions = data.deductions;
+            console.log(data);
+        }).
+        error(function(data, status, headers, config) {
+            console.log("ERROR WITH HTTP CALL");
+        });
+
+        // $scope.deductions = [{
+        //     'name': 'Nexus S',
+        //     'shortDiscription': 'Fast just got faster with Nexus S.'
+        // }, {
+        //     'name': 'Motorola XOOM™ with Wi-Fi',
+        //     'shortDiscription': 'The Next, Next Generation tablet.'
+        // }, {
+        //     'name': 'MOTOROLA XOOM™',
+        //     'shortDiscription': 'The Next, Next Generation tablet.'
+        // }];
 
         console.log('deduction state');
     });
