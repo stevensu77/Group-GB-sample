5fc2375b464e8ac2360800ca87814cb9fdeff17f
Luo Dong <luodong89@gmail.com>
2015-07-09 00:04:38 -0400

    Adjust Root Page Head Bar and logout button

--- app/scripts/controllers/deductionCtrl.js
+++ app/scripts/controllers/deductionCtrl.js
@@ -2,9 +2,9 @@
 
 angular.module('gbApp')
   .controller('deductionCtrl', function($scope, getUserService) {
-    getUserService.getUser().$promise.then(function(response) {
-      $scope.userName = response.userName;
-    });
+    // getUserService.getUser().$promise.then(function(response) {
+    //   $scope.userName = response.userName;
+    // });
 
     console.log('deduction state');
   });
--- app/scripts/controllers/rootCtrl.js
+++ app/scripts/controllers/rootCtrl.js
@@ -1,9 +1,13 @@
 'use strict';
 
 angular.module('gbApp')
-  .controller('rootCtrl', function($scope, loginService) {
+  .controller('rootCtrl', function($scope, loginService, getUserService) {
     console.log('root state');
+    getUserService.getUser().$promise.then(function(response) {
+      $scope.userName = response.userName;
+    });
     $scope.logout = function(){
     	loginService.logout();
     }
+    $scope.myDate = new Date();
   });
