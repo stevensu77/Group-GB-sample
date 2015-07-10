996bfccfb2eac95d77c20c72386e6032b7a04bd9
Luo Dong <luodong89@gmail.com>
2015-07-10 12:57:32 -0400

    Fix table header, delete and filter bugs.

--- app/scripts/controllers/deductionCtrl.js
+++ app/scripts/controllers/deductionCtrl.js
@@ -23,11 +23,37 @@ angular.module('gbApp')
             console.log("ERROR WITH HTTP CALL");
         });
 
-        $scope.deleteDeduction = function(index){
+        $scope.deleteDeduction = function(removeItem){
+            var products = $scope.deductions;
+            var length = products.length;
+            var removeIndex = -1;
+            for(var ii=0; ii<length; ii++){
+                if(angular.equals(products[ii], removeItem)){
+                    removeIndex = ii;
+                    break;
+                }
+            }
         	if(window.confirm("Are U sure to delete?")){
-        		$scope.deductions.splice(index,1);
+        		$scope.deductions.splice(removeIndex,1);
         	}
         }
+
+        // $scope.deleteDeduction = function(index){
+        //     var did = $scope.deductions[index].id;
+        //     if(window.confirm("Are U sure to delete?")){
+        //     //  $scope.deductions.splice(index,1);
+        //     $http.delete("/api/deduction/:id", {id:did})
+        //     .success(function(res){
+        //        // console.log(res);
+        //        if(res.status == "success"){
+        //           $scope.deductions.splice(index,1);
+        //           alert("Delete succeeds!");
+        //        }
+        //     });
+        //     }
+        // }
+
+
 
 
         var indexFlag = 0;
@@ -41,14 +67,25 @@ angular.module('gbApp')
         //    console.log("add box");
         
         }
-        $scope.editDeduction = function(index){
+        $scope.editDeduction = function(Item){
             $scope.editbool = !$scope.editbool;
             $scope.editOriPos = '-100px';
-            indexFlag = index;
-            $scope.editName = $scope.deductions[index].deductionName;
-            $scope.editShort = $scope.deductions[index].longDescription;
-            $scope.editLong = $scope.deductions[index].shortDescription;
-            $scope.editValue = $scope.deductions[index].value;
+            indexFlag = -1;
+
+            var products = $scope.deductions;
+            var length = products.length;
+
+            for(var ii=0; ii<length; ii++){
+                if(angular.equals(products[ii], Item)){
+                    indexFlag = ii;
+                    break;
+                }
+            }
+
+            $scope.editName = Item.deductionName;
+            $scope.editShort = Item.longDescription;
+            $scope.editLong = Item.shortDescription;
+            $scope.editValue = Item.value;
             
         }        
         $scope.cancelDeduction = function(){
--- gbServer/routes/login.js
+++ gbServer/routes/login.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
-  if(req.body.userName === 'Todd' && req.body.password === '123456') {
+  if((req.body.userName === 'Todd' && req.body.password === '123456') ||
+     (req.body.userName === 'Eric' && req.body.password === '123456')) {
     res.send({
       authentication: 'success'
     });
